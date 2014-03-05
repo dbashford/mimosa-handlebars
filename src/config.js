423fe399ec0d60d014f1219f93187fa8eca54d47
david.bashford <dbashford@bericotechnologies.com>
2014-03-05 07:27:02 -0500

    config to js, eslinting, leading comments removed

--- src/config.js
+++ src/config.js
@@ -4,18 +4,18 @@ exports.defaults = function() {
   return {
     handlebars: {
       extensions: ["hbs", "handlebars"],
-      helpers:["app/template/handlebars-helpers"],
+      helpers:["app/template/handlebars-helpers"]
     }
   };
 };
 
 exports.placeholder = function() {
-  return "\t\n\n"+
-         "  # handlebars:              # config settings for the Handlebars compiler module\n" +
-         "    # lib: undefined         # use this property to provide a specific version of Handlebars\n" +
-         "    # extensions: [\"hbs\", \"handlebars\"]  # default extensions for Handlebars files\n" +
-         "    # helpers:[\"app/template/handlebars-helpers\"]  # the paths from watch.javascriptDir to\n" +
-         "                             # the files containing handlebars helper/partial registrations\n";
+  return "\t\n\n" +
+         "  handlebars:              # config settings for the Handlebars compiler module\n" +
+         "    lib: undefined         # use this property to provide a specific version of Handlebars\n" +
+         "    extensions: [\"hbs\", \"handlebars\"]  # default extensions for Handlebars files\n" +
+         "    helpers:[\"app/template/handlebars-helpers\"]  # the paths from watch.javascriptDir to\n" +
+         "                           # the files containing handlebars helper/partial registrations\n";
 };
 
 exports.validate = function( config, validators ) {
@@ -24,7 +24,7 @@ exports.validate = function( config, validators ) {
   if ( validators.ifExistsIsObject( errors, "handlebars config", config.handlebars ) ) {
 
     if ( !config.handlebars.lib ) {
-      config.handlebars.lib = require( 'handlebars' );
+      config.handlebars.lib = require( "handlebars" );
     }
 
     if ( validators.isArrayOfStringsMustExist( errors, "handlebars.extensions", config.handlebars.extensions ) ) {
